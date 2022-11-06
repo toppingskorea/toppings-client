f336f92b577e3f3135380c33285d858267282d89
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-06 22:51:26 +0900

    refactor: remove unused file

--- src/constants/Variables.ts
+++ src/constants/Variables.ts
@@ -1,21 +1,3 @@
-// 데스크톱
-export const XD_DESKTOP_WIDTH = 1920;
-export const XD_DESKTOP_HEIGHT = 1080;
-
-// 노트북
-export const XD_LAPTOP_WIDTH = 1024;
-
-// 태블릿
-export const XD_TABLET_WIDTH = 768;
-
-// 모바일
-export const XD_MOBILE_WIDTH = 384;
-export const XD_MOBILE_HEIGHT = 844;
-
-export const ROOT_FONT_SIZE = 10;
-
-// Mo에서 폰트 사이즈 배율 조정하는 값
-
 export const LIGHT_WEIGHT = 300;
 export const NORMAL_WEIGHT = 400;
 export const BOLD_WEIGHT = 500;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-export { default as media } from "./Media";
 export { default as Text } from "./Typography";
 export * from "./Variables";
 export { default as env } from "./Env";
