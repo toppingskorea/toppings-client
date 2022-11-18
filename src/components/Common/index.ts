6faeb20a092e2378ff6b155b089c2acd8094f6af
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-18 22:35:41 +0900

    fix: 불필요한 Slider 컴포넌트 삭제

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -5,6 +5,5 @@ export {
 export { default as SSRSafeSuspense } from "./SSRSafeSuspense";
 export { default as InternalLink } from "./InternalLink";
 export { default as PageLoader } from "./PageLoader";
-export { default as Slider } from "./Slider";
 export * from "./Button";
 export * from "./TextField";
