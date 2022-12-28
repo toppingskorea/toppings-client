c10f23f5f92e381e471f0abee0b992992b5526e5
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-12-28 23:05:58 +0900

    fix: UseQueryOptions => UseMutationsOptions in register

--- apps/client/server/register/mutations.ts
+++ apps/client/server/register/mutations.ts
@@ -1,7 +1,16 @@
-import type { UseQueryOptions } from "@tanstack/react-query";
-import { useMutation } from "@tanstack/react-query";
+/* eslint-disable import/prefer-default-export */
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
 import { register } from "./apis";
 
-export const useRegister = (options: Pick<UseQueryOptions, "onSuccess">) => {
+export const useRegister = (
+  options: Pick<
+    UseMutationOptions<
+      Awaited<ReturnType<typeof register>>,
+      unknown,
+      Parameters<typeof register>[0]
+    >,
+    "onSuccess"
+  >
+) => {
   return useMutation(register, options);
 };
