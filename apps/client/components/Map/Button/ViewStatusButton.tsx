3ad8bd6e3515af03fbc097911cebe807e9dfad8c
po4tion <po4tion0429@gmail.com>
2023-01-06 13:02:06 +0900

    fix: toss padding util 함수 사용

--- apps/client/components/Map/Button/ViewStatusButton.tsx
+++ apps/client/components/Map/Button/ViewStatusButton.tsx
@@ -1,6 +1,6 @@
 import { css, useTheme } from "@emotion/react";
 import { List } from "@svgs/map";
-import { flex, position } from "@toss/emotion-utils";
+import { flex, padding, position } from "@toss/emotion-utils";
 import { motion } from "framer-motion";
 import { Text } from "~/components/Common/Typo";
 import { defaultScaleChangeVariants, framerMocker } from "~/constants";
@@ -25,7 +25,10 @@ const ViewStatusButton = ({ text, onClick }: Props) => {
         })}
         left: 40%;
         ${flex("center")}
-        padding: 8px 21px;
+        ${padding({
+          x: 21,
+          y: 8
+        })}
         border-radius: 20px;
         background-color: ${colors.white};
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
