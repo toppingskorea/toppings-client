6a58bc7c6e1e9b07e43b0e3bdb4a68b9e199732d
chanhyukpark-29cm <chanhyuk-tech@29cm.co.kr>
2023-01-09 21:03:34 +0900

    hotfix: svg 대문자 => 소문자

--- apps/client/assets/svgs/recent/index.ts
+++ apps/client/assets/svgs/recent/index.ts
@@ -1,4 +1,4 @@
 export { default as Exit } from "./exit.svg";
 export { default as RemoveHistory } from "./removeHistory.svg";
-export { default as SmallExit } from "./SmallExit.svg";
+export { default as SmallExit } from "./smallExit.svg";
 export { default as Timeline } from "./timeline.svg";
