f9226df27f6d051228f781410ccf501125f291d9
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-14 22:53:56 +0900

    fix(admin): overview skeleton 개선

--- apps/admin/pages/overview.page.tsx
+++ apps/admin/pages/overview.page.tsx
@@ -1,7 +1,9 @@
+import { HStack } from "@chakra-ui/react";
 import { Suspense } from "@suspensive/react";
 import { CountCards } from "~/components/Overview";
 import Skeleton from "~/components/Skeleton";
 import { useSetHeader } from "~/hooks";
+import { generateComponent } from "~/utils";
 
 const Overview = () => {
   useSetHeader("Overview");
@@ -10,12 +12,17 @@ const Overview = () => {
     <div>
       <Suspense.CSROnly
         fallback={
-          <Skeleton.Box
-            size={{
-              width: 100,
-              height: 50
-            }}
-          />
+          <HStack gap={14}>
+            {generateComponent(
+              <Skeleton.Box
+                size={{
+                  width: 100,
+                  height: 50
+                }}
+              />,
+              3
+            )}
+          </HStack>
         }
       >
         <CountCards />
