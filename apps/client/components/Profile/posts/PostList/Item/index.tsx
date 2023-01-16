e20beac2f5afeef95e9f87ddc8afa844c160c9c2
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-17 01:17:01 +0900

    feat(client): 나의 음식점 구현 완료

--- apps/client/components/Profile/posts/PostList/Item/index.tsx
+++ apps/client/components/Profile/posts/PostList/Item/index.tsx
@@ -4,7 +4,6 @@ import {
   flex,
   gutter,
   padding,
-  size,
   Spacing,
   Stack,
   touchable
@@ -28,9 +27,9 @@ const Item = ({ post }: Props) => {
     <li
       key={post.id}
       css={css`
-        ${flex({ direction: "column", align: "center" })}
-        ${gutter({ direction: "vertical", space: 24 })}
-        ${padding({ top: 16, bottom: 8, x: 4 })}
+        ${flex({ align: "flex-end" })}
+        ${gutter({ direction: "horizontal", space: 18 })}
+        ${padding({ top: 14, bottom: 14, x: 4 })}
         ${touchable}
         border-bottom: 1px solid ${colors.secondary.D9};
       `}
@@ -39,41 +38,32 @@ const Item = ({ post }: Props) => {
       <Image
         src={post.thumbnail}
         alt={post.description}
-        width={344}
-        height={174}
+        width={164}
+        height={164}
         css={css`
+          min-width: 164px;
           border-radius: 20px;
           box-shadow: 0 4px 4px ${hexToRgba(colors.black, 0.25)};
         `}
       />
 
-      <Stack.Vertical
-        css={css`
-          ${size({
-            width: 344
-          })}
-        `}
-        gutter={4}
-      >
-        <Flex justify="space-between">
-          <Flex align="flex-end">
-            <Text _fontSize={16} weight={weighs.semiBold}>
-              {post.name}
-            </Text>
-            <Spacing size={6} direction="horizontal" />
-            <Text _fontSize={12} _color={colors.secondary["49"]}>
-              {post.type}
-            </Text>
-          </Flex>
-          <HeartWithNumber like={post.like} likeCount={post.likeCount} />
+      <Stack.Vertical gutter={4}>
+        <Flex align="flex-end">
+          <Text _fontSize={15} weight={weighs.semiBold}>
+            {post.name}
+          </Text>
+          <Spacing size={6} direction="horizontal" />
+          <Text _fontSize={11} _color={colors.secondary["83"]}>
+            {post.type}
+          </Text>
         </Flex>
-        <Flex justify="space-between">
-          <Text _fontSize={10} _color={colors.secondary[34]}>
+
+        <Flex justify="space-between" align="center">
+          <Text _fontSize={11} _color={colors.secondary["83"]}>
             {post.address}
           </Text>
-          <Text _fontSize={10} _color={colors.secondary[34]}>
-            by {post.writer}
-          </Text>
+          <Spacing direction="horizontal" size={6} />
+          <HeartWithNumber like={post.like} likeCount={post.likeCount} />
         </Flex>
       </Stack.Vertical>
     </li>
