1171ac2fb540048addf7ba68b7fd549ec8074ff5
po4tion <po4tion0429@gmail.com>
2023-01-19 23:04:33 +0900

    feat: Recent type update

--- apps/client/@types/recent.d.ts
+++ apps/client/@types/recent.d.ts
@@ -1,4 +1,13 @@
 declare module Recent {
+  interface HistoryItem {
+    id: number;
+    keyword: string;
+    type: string;
+    category: string;
+    content?: string;
+    restaurantId?: number;
+  }
+
   interface HistoryDTO {
     id: number;
     keyword: string;
