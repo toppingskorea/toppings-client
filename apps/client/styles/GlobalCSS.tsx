e409d53bb87d416db99a151a2adb110b9d6ff90e
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-20 20:53:06 +0900

    fix(client): 폰트 원복

--- apps/client/styles/GlobalCSS.tsx
+++ apps/client/styles/GlobalCSS.tsx
@@ -17,9 +17,9 @@ const GlobalCSS = () => {
         input,
         textarea,
         button {
-          /* font-family: Inter, Pretendard, -apple-system, BlinkMacSystemFont,
+          font-family: Inter, Pretendard, -apple-system, BlinkMacSystemFont,
             Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-            Helvetica Neue, sans-serif; */
+            Helvetica Neue, sans-serif;
         }
 
         // 화면을 꽉 채우기 위해서
