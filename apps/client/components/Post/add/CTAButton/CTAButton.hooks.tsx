366d692d6d68c9c15dc29b766a46b19885dda593
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-22 01:30:38 +0900

    fix(client): excute => execute

--- apps/client/components/Post/add/CTAButton/CTAButton.hooks.tsx
+++ apps/client/components/Post/add/CTAButton/CTAButton.hooks.tsx
@@ -12,29 +12,29 @@ const useSubmitVerification = ({
 >) => {
   const overlay = useOverlay();
 
-  const excuteCautionOverlay = (cautionMessage: string) =>
+  const executeCautionOverlay = (cautionMessage: string) =>
     overlay.open(({ exit }) => (
       <AlertModal information={cautionMessage} exitFn={exit} />
     ));
 
   const verificationSubmitInClient = () => {
     if (images?.length === 0) {
-      excuteCautionOverlay(`It can't be uploaded\nwithout pictures`);
+      executeCautionOverlay(`It can't be uploaded\nwithout pictures`);
       return true;
     }
 
     if (!name) {
-      excuteCautionOverlay(`It can't be uploaded\nwithout restaurant`);
+      executeCautionOverlay(`It can't be uploaded\nwithout restaurant`);
       return true;
     }
 
     if (!description) {
-      excuteCautionOverlay(`It can't be uploaded\nwithout description`);
+      executeCautionOverlay(`It can't be uploaded\nwithout description`);
       return true;
     }
 
     if (!type) {
-      excuteCautionOverlay(`It can't be uploaded\nwithout category`);
+      executeCautionOverlay(`It can't be uploaded\nwithout category`);
       return true;
     }
 
