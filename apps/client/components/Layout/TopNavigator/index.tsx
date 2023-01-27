89159ad681589a52d8054551469d9e63ff1e792e
po4tion <po4tion0429@gmail.com>
2023-01-27 14:39:40 +0900

    fix: TopNavigator 조건문을 통과하여 존재하는 값은 optional chaining이 필요 없습니다.

--- apps/client/components/Layout/TopNavigator/index.tsx
+++ apps/client/components/Layout/TopNavigator/index.tsx
@@ -24,7 +24,7 @@ const TopNavigator = () => {
     }
 
     if (state.top?.backDirectlyURL) {
-      push(state.top?.backDirectlyURL);
+      push(state.top.backDirectlyURL);
       return;
     }
 
