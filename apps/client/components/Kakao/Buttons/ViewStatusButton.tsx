8273415f5cc8a21ec59f0f464b293170f429fd34
po4tion <po4tion0429@gmail.com>
2023-01-27 12:03:16 +0900

    fix: ViewStatusButton dim color는 hextorgba function을 사용합니다.

--- apps/client/components/Kakao/Buttons/ViewStatusButton.tsx
+++ apps/client/components/Kakao/Buttons/ViewStatusButton.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import type { FC, SVGProps } from "react";
 import { Text } from "~/components/Common/Typo";
 import { defaultScaleChangeVariants, framerMocker } from "~/constants";
+import { hexToRgba } from "~/utils";
 
 interface Props {
   text: string;
@@ -34,7 +35,7 @@ const ViewStatusButton = ({ text, onClick, Icon }: Props) => {
         border-radius: 20px;
         background-color: ${colors.white};
         transform: translateX(-50%) !important;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        box-shadow: 0px 4px 4px ${hexToRgba(colors.black, 0.25)};
         z-index: ${zIndex.two};
       `}
     >
