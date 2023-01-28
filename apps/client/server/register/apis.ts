00590f82091c17a20e1eaf6b7dd4e3e54f3821c0
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-28 17:25:54 +0900

    fix(client): 회원가입시 habits와 habit이 혼동된 현상을 해결합니다

--- apps/client/server/register/apis.ts
+++ apps/client/server/register/apis.ts
@@ -1,12 +1,13 @@
 import { authRequest } from "~/constants";
 
+// eslint-disable-next-line import/prefer-default-export
 export const register = (payload: {
   country: string;
-  habit?: { title: string; content: string }[];
+  habits?: { title: string; content: string }[];
 }) => {
   return authRequest.post("/v1/user", {
     country: payload.country,
-    habit: payload.habit?.map(_habit => ({
+    habit: payload.habits?.map(_habit => ({
       title: _habit.title,
       content: _habit.content.replace(" ", "")
     }))
