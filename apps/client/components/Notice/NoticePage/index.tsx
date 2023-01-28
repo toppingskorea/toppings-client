2d5318cd8f79743779e4330fdda785a6aaf50b19
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-29 02:29:21 +0900

    fix(client):Notice와 관련된  페이지 컴포넌트와, page 계층 분리 완료함

---
+++ apps/client/components/Notice/NoticePage/index.tsx
@@ -0,0 +1,54 @@
+import { useTheme } from "@emotion/react";
+import { Suspense } from "@suspensive/react";
+import { Stack } from "@toss/emotion-utils";
+import { Text } from "~/components/Common/Typo";
+import Skeleton from "~/components/Skeleton";
+import { OpenGraph } from "~/components/Util";
+import { useSetNavigation } from "~/hooks";
+import { generateComponent } from "~/utils";
+import NotificationList from "./NotificationList";
+
+const Notice = () => {
+  const { colors, weighs } = useTheme();
+  // const setNoticeActivate = useNoticeActivateSetter();
+
+  // useEffect(() => {
+  //   setNoticeActivate(false);
+  //   return () => setNoticeActivate(false);
+  // }, [setNoticeActivate]);
+
+  useSetNavigation({
+    top: {
+      title: (
+        <Text _fontSize={19} weight={weighs.bold} _color={colors.secondary[47]}>
+          Notifications
+        </Text>
+      )
+    },
+    bottom: true
+  });
+  return (
+    <div>
+      <OpenGraph title="Notice" />
+      <Suspense.CSROnly
+        fallback={
+          <Stack.Vertical>
+            {generateComponent(
+              <Skeleton.Box
+                size={{
+                  width: "100%",
+                  height: 40
+                }}
+              />,
+              6
+            )}
+          </Stack.Vertical>
+        }
+      >
+        <NotificationList />
+      </Suspense.CSROnly>
+    </div>
+  );
+};
+
+export default Notice;
