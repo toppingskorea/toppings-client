2a10a4a1f5eb23e36a5e24f2df6c47ae4633f70e
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-29 02:34:21 +0900

    fix(client): about , login 과 관련된  페이지 컴포넌트와, page 계층 분리 완료함

--- apps/client/pages/login/index.page.tsx
+++ apps/client/pages/login/index.page.tsx
@@ -1,23 +1,3 @@
-import { useRouter } from "next/router";
-import React from "react";
-import { env } from "~/constants";
-import { Text } from "~/components/Common/Typo";
+import { LoginPage } from "~/components/Login";
 
-const Login = () => {
-  const router = useRouter();
-
-  const kakaoUrl = `${env.TOPPINGS_SERVER_URL}/oauth2/authorization/kakao?redirect_uri=${env.REDIRECT_URI}`;
-
-  const handleClickLogin = () => {
-    router.replace(kakaoUrl);
-  };
-  return (
-    <div>
-      <button type="button" onClick={handleClickLogin}>
-        <Text _fontSize={24}>로그인하기</Text>
-      </button>
-    </div>
-  );
-};
-
-export default Login;
+export default LoginPage;
