eb1d987aaa376c134cc95bd55a360960c10b4552
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-28 17:33:56 +0900

    fix(client): 맵 관련 페이지의 topnavigator를 수정합니다

--- apps/client/pages/recent/eatingHabit.page.tsx
+++ apps/client/pages/recent/eatingHabit.page.tsx
@@ -3,7 +3,9 @@ import {
   useCurrentSelectCategorySetter,
   useCurrentSelectKeywordSetter
 } from "@atoms/index";
+import { useTheme } from "@emotion/react";
 import { useRouter } from "next/router";
+import { Text } from "~/components/Common/Typo";
 import { TagFamily } from "~/components/Recent";
 import { SelectEatingHabit } from "~/components/Section";
 import { OpenGraph } from "~/components/Util";
@@ -13,10 +15,16 @@ import { useUploadRecentHistory } from "~/server/recent";
 import { replaceSpace } from "~/utils";
 
 const EatingHabitPage = () => {
+  const { colors, weighs } = useTheme();
   useSetNavigation({
     top: {
       marginBottom: 37,
-      backDirectlyURL: "/recent"
+      backDirectlyURL: "/recent",
+      title: (
+        <Text _fontSize={19} weight={weighs.bold} _color={colors.secondary[47]}>
+          Select a Eating Habit
+        </Text>
+      )
     }
   });
 
