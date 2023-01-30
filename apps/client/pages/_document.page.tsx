145b1937aab36ceedb1880703923a78bd620946c
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-31 01:40:33 +0900

    feat(client): 파비콘 설정 완료

--- apps/client/pages/_document.page.tsx
+++ apps/client/pages/_document.page.tsx
@@ -14,7 +14,6 @@ export default class MyDocument extends Document<DocumentPropTypes> {
     return (
       <Html lang="ko">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             type="text/css"
@@ -29,6 +28,84 @@ export default class MyDocument extends Document<DocumentPropTypes> {
             type="text/javascript"
             src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${env.KAKAO_JAVASCRIPT_KEY}&libraries=services`}
           />
+
+          {/* favicon start */}
+
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* favicon end */}
 
           <Script strategy="afterInteractive">{`console.log("%c" + "       _       _                       __  __       _          _______                _                 _ \\n      | |     (_)                     |  \\\\/  |     | |        |__   __|              (_)               | |\\n      | | ___  _ _ __    _   _ ___    | \\\\  / | __ _| | _____     | | ___  _ __  _ __  _ _ __   __ _ ___| |\\n  _   | |/ _ \\\\| | '_ \\\\  | | | / __|   | |\\\\/| |/ _\` | |/ / _ \\\\    | |/ _ \\\\| '_ \\\\| '_ \\\\| | '_ \\\\ / _\` / __| |\\n | |__| | (_) | | | | | | |_| \\\\__ \\\\_  | |  | | (_| |   <  __/    | | (_) | |_) | |_) | | | | | (_| \\\\__ \\\\_|\\n  \\\\____/ \\\\___/|_|_| |_|  \\\\__,_|___(_) |_|  |_|\\\\__,_|_|\\\\_\\\\___|    |_|\\\\___/| .__/| .__/|_|_| |_|\\\\__, |___(_)\\n                                                                         | |   | |             __/ |      \\n                                                                         |_|   |_|            |___/       ",
         "color: #FF7D1F; font-size: 7px;");`}</Script>
