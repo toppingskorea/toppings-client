fcf330b4c50fcc4779f6b540140fe783c6f5a036
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-01-31 00:09:32 +0900

    feat(client): About페이지 퍼블리싱을 진행합니다

---
+++ apps/client/components/About/AboutPage/AboutPage.constants.ts
@@ -0,0 +1,12 @@
+const EXTERNAL_LINK_LIST = [
+  {
+    label: "Privacy Policy",
+    href: "https://plip.kr/html/95264de9-5e3f-4f0c-9211-b862b6babac2.html"
+  },
+  {
+    label: "Terms of Use",
+    href: "https://abaft-pie-111.notion.site/82a3453aee2d493596b3f7f7f60b16e9"
+  }
+] as const;
+
+export default EXTERNAL_LINK_LIST;
