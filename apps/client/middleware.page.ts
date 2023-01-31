adce01f03a5ef5023b8572123435636ce727a6c1
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-02-01 03:26:37 +0900

    feat(client): 로그인한 유저만 가능한 액션을 제어할 수 있는 훅 구성완료

--- apps/client/middleware.page.ts
+++ apps/client/middleware.page.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import type { NextMiddleware } from "next/server";
 import { NextResponse } from "next/server";
 import { env } from "~/constants";
+import { KAKAO_LOGIN_REDIRECT_URI } from "./hooks/auth/useProtectRouteModal";
 import { verifyToken } from "./utils";
 
 // 로그인한 유저만 접근할 수 있는 라우트
@@ -49,9 +50,18 @@ const middleware: NextMiddleware = async request => {
     const token = request.nextUrl.searchParams.get("accessToken");
     const role = request.nextUrl.searchParams.get("role");
 
+    // 이미 가입한 유저인 경우, 어디서 로그인 했는지에 따라서 그쪽으로 보내줘야함
+    const justLoginRedirectUri = request.cookies.get(
+      KAKAO_LOGIN_REDIRECT_URI
+    )?.value;
+
+    const existingUserRedirectUri = justLoginRedirectUri ?? "/map";
+
     const response = NextResponse.redirect(
       new URL(
-        role === "ROLE_TEMP" ? "/register/nationality" : "/map",
+        role === "ROLE_TEMP"
+          ? "/register/nationality"
+          : existingUserRedirectUri,
         request.url
       )
     );
