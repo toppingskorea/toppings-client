3cd75836c0fd7899a6f48e90d5cf5822f6b85cd5
Donggyu Kim <po4tion0429@gmail.com>
2023-02-03 00:56:18 +0900

    feat(client): PWA를 적용합니다. (#105)
    * feat: STEP1. withPWA 적용하기
    
    * feat: PWA를 적용합니다.

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,1 +1,8 @@
-module.exports = require("@toppings/next-config/next.config");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require("next-pwa")({
+  dest: "public"
+});
+
+const config = require("@toppings/next-config/next.config");
+
+module.exports = withPWA(config);
