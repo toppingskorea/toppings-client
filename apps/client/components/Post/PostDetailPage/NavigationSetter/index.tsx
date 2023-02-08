2adc63a3e85629b90ad457e7a197cba774b79ccc
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-02-08 21:00:29 +0900

    fix(client): 단축문법 적용

--- apps/client/components/Post/PostDetailPage/NavigationSetter/index.tsx
+++ apps/client/components/Post/PostDetailPage/NavigationSetter/index.tsx
@@ -1,10 +1,10 @@
 import { useTheme } from "@emotion/react";
+import { grayNoRingLogo } from "@images/common";
+import { Edit } from "@svgs/common";
 import { Flex, Spacing } from "@toss/emotion-utils";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
-import { grayNoRingLogo } from "~/assets/images/common";
-import { Edit } from "~/assets/svgs/common";
 import { CircleCountry } from "~/components/Common";
 import { Text } from "~/components/Common/Typo";
 import { useSetNavigation } from "~/hooks";
