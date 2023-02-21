aab9f668a0057fff759ea13446731e7a6b413d5d
박찬혁 <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2023-02-22 01:43:57 +0900

    fix(client): profile 페이지 진입시 bottomnavigation이 깜빡거리는 현상을 해결합니다 (#160)

--- apps/client/components/Profile/ProfilePage/index.tsx
+++ apps/client/components/Profile/ProfilePage/index.tsx
@@ -1,50 +1,74 @@
 import { css } from "@emotion/react";
 import { Suspense } from "@suspensive/react";
+import { Hamburger } from "@svgs/common";
 import { padding, Stack } from "@toss/emotion-utils";
 import Skeleton from "~/components/Skeleton";
+import { useInternalRouter, useSetNavigation } from "~/hooks";
 import { generateComponent } from "~/utils";
 import EditButton from "./EditButton";
 import Info from "./Info";
 
-const ProfilePage = () => (
-  <section
-    css={css`
-      ${padding({ x: 25 })}
-    `}
-  >
-    <Suspense.CSROnly
-      fallback={
-        <Stack.Vertical
-          css={css`
-            padding-top: 140px;
-          `}
-        >
-          <Stack.Horizontal align="center">
-            <Skeleton.Circle size={78} />
+const ProfilePage = () => {
+  const { push } = useInternalRouter();
+  useSetNavigation({
+    top: {
+      right: {
+        element: <Hamburger />,
+        onClick: () => push("/profile/menu")
+      },
+      title: (
+        <Skeleton.Box
+          size={{
+            width: 60,
+            height: 20
+          }}
+        />
+      ),
+      marginBottom: 33
+    },
+    bottom: true
+  });
 
-            <Stack.Horizontal>
-              {generateComponent(
-                <Skeleton.Box
-                  size={{
-                    width: 50,
-                    height: 50
-                  }}
-                />,
-                3
-              )}
-            </Stack.Horizontal>
-          </Stack.Horizontal>
-
-          <Stack.Vertical>
-            {generateComponent(<Skeleton.Paragraph />, 2)}
-          </Stack.Vertical>
-        </Stack.Vertical>
-      }
+  return (
+    <section
+      css={css`
+        ${padding({ x: 25 })}
+      `}
     >
-      <Info />
-    </Suspense.CSROnly>
+      <Suspense.CSROnly
+        fallback={
+          <Stack.Vertical
+            css={css`
+              padding-top: 140px;
+            `}
+          >
+            <Stack.Horizontal align="center">
+              <Skeleton.Circle size={78} />
 
-    <EditButton />
-  </section>
-);
+              <Stack.Horizontal>
+                {generateComponent(
+                  <Skeleton.Box
+                    size={{
+                      width: 50,
+                      height: 50
+                    }}
+                  />,
+                  3
+                )}
+              </Stack.Horizontal>
+            </Stack.Horizontal>
+
+            <Stack.Vertical>
+              {generateComponent(<Skeleton.Paragraph />, 2)}
+            </Stack.Vertical>
+          </Stack.Vertical>
+        }
+      >
+        <Info />
+      </Suspense.CSROnly>
+
+      <EditButton />
+    </section>
+  );
+};
 export default ProfilePage;
