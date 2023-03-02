3344e4e519df6e1ae0e501b48b8840e253f4c078
po4tion <po4tion0429@gmail.com>
2023-03-03 08:58:39 +0900

    feat(client) : 지도에 내 위치를 표시합니다.

--- apps/client/assets/json/index.ts
+++ apps/client/assets/json/index.ts
@@ -1,5 +1,6 @@
 export { default as check } from "./check.json";
 export { default as destination } from "./destination.json";
+export { default as myLocation } from "./myLocation.json";
 export { default as pin } from "./pin.json";
 export { default as rocket } from "./rocket.json";
 export { default as star } from "./star.json";
