631da2fd72513317606444491b444d31607a9355
po4tion <po4tion0429@gmail.com>
2023-03-06 22:45:03 +0900

    feat(client) : constants 폴더의 default export를 named export로 변경합니다.

--- apps/client/constants/Env.ts
+++ apps/client/constants/Env.ts
@@ -1,4 +1,4 @@
-const env = {
+export const env = {
   KAKAO_JAVASCRIPT_KEY: process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY,
   REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
   SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
@@ -10,5 +10,3 @@ const env = {
   GOOGLE_ANALYTICS_TRACKING_ID:
     process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID
 } as const;
-
-export default env;
