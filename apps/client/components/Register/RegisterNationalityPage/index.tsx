cd6035f31daae7186c7b7c31b492ed825181be72
박찬혁 <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2023-03-13 23:22:35 +0900

    refactor(client): register 관련 로직을 recoil에서 zustand로 이관합니다. 또한 register관련 로직의 UI와 비즈니스 로직을 분리합니다 (#204)
    * refactor(client): recoil을 zustand로 이관함
    
    * refactor(client): 불필요한레지스토ㅓㅏ 관련 로직삭제

--- apps/client/components/Register/RegisterNationalityPage/index.tsx
+++ apps/client/components/Register/RegisterNationalityPage/index.tsx
@@ -1,45 +1,24 @@
-import { useRegisterState } from "@atoms/index";
 import { css, useTheme } from "@emotion/react";
 import { useInput } from "@toppings/hooks";
 import { padding, position, width100 } from "@toss/emotion-utils";
 import { SearchInput } from "~/components/Common";
-import { Text } from "~/components/Common/Typo";
 import { SearchNationality } from "~/components/Section";
 import { OpenGraph } from "~/components/Util";
 import { useDeviceInfo } from "~/contexts";
+import { useBlurController } from "~/hooks";
 import {
-  useBlurController,
-  useInternalRouter,
+  useCountryClick,
   useSetNavigation
-} from "~/hooks";
+} from "./RegisterNationalityPage.hooks";
 
 const RegisterNationalityPage = () => {
-  const { push } = useInternalRouter();
   const { isIos } = useDeviceInfo();
-  const { colors, weighs, dimensions } = useTheme();
-  const [register, setRegister] = useRegisterState();
-
+  const { colors, dimensions } = useTheme();
   const { focusController, isFocused } = useBlurController();
-
   const isIosFocused = isIos && isFocused;
+  useSetNavigation(isIosFocused);
 
-  useSetNavigation({
-    top: isIosFocused
-      ? undefined
-      : {
-          marginBottom: 35,
-          title: (
-            <Text
-              _fontSize={19}
-              weight={weighs.bold}
-              _color={colors.secondary[47]}
-            >
-              Select a Nationality
-            </Text>
-          ),
-          hideBackButton: true
-        }
-  });
+  const { onCountryClickHandler } = useCountryClick();
 
   const { props: keyword, setValue } = useInput({});
 
@@ -48,10 +27,7 @@ const RegisterNationalityPage = () => {
       <OpenGraph title="Register Nationality" />
       <SearchNationality
         keyword={keyword.value}
-        onCountryClick={name => {
-          setRegister({ ...register, country: name });
-          push("/register/eatingHabits");
-        }}
+        onCountryClick={onCountryClickHandler}
         isFixedPosition={isIosFocused}
       />
 
--- apps/client/recoil/atoms/index.ts
+++ apps/client/recoil/atoms/index.ts
@@ -2,4 +2,3 @@ export * from "./edit";
 export * from "./navigation";
 export * from "./post";
 export * from "./recent";
-export * from "./register";
